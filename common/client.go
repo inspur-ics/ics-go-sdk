c1e5e7dd045f82d7fd6fd578de109251f70619e1
Yongchao Wang <wangyongchao.bj@inspur.com>
2020-01-08 08:47:12 +0800

    add task trace process

--- common/client.go
+++ common/client.go
@@ -5,14 +5,42 @@ import (
     "github.com/inspur-ics/ics-go-sdk/client/methods"
     "github.com/inspur-ics/ics-go-sdk/client/restful"
     "github.com/inspur-ics/ics-go-sdk/client/types"
+    "sync"
+    "time"
 )
+
+
 
 type RestAPI struct {
     RestAPITripper restful.RestAPITripper
 }
 
-func(r *RestAPI) TraceTaskProcess(task *types.Task) (*types.TaskInfo, error) {
+func (r *RestAPI) TraceTaskProcess(task *types.Task) (*types.TaskInfo, error) {
+    var wg sync.WaitGroup
+    wg.Add(1)
+    go r.tracing(task, wg)
+    wg.Wait()
+
     ctx := context.Background()
     taskInfo, err := methods.GetTaskInfo(ctx, r.RestAPITripper, task)
     return taskInfo, err
+}
+
+func (r *RestAPI) tracing(task *types.Task, wg sync.WaitGroup) {
+    defer wg.Done()
+    ctx := context.Background()
+    count := 1
+    for {
+        taskInfo, err := methods.GetTaskInfo(ctx, r.RestAPITripper, task)
+        if err != nil {
+            break
+        }
+        if taskInfo == nil || taskInfo.State == "FINISHED" || taskInfo.State == "ERROR" {
+            break
+        }
+        if count > 72000 {
+            break
+        }
+        time.Sleep(100 * time.Millisecond)
+    }
 }
