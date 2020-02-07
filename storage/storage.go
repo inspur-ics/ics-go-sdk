3ab0446506e1c9b4a60af39209f205a05772e77b
Yongchao Wang <wangyongchao.bj@inspur.com>
2020-02-07 11:53:08 +0800

    fix bug for sun codes merge.

--- storage/storage.go
+++ storage/storage.go
@@ -7,12 +7,26 @@ import (
 )
 
 //FIXME TODO.WANGYONGCHAO
+func (sts *StorageService)GetAllDatastores(ctx context.Context, datacenterId string) ([]*types.Storage, error) {
+    var storages []*types.Storage
+    var err error
+
+    return storages, err
+}
+
 func (sts *StorageService)GetStoragesList(ctx context.Context) ([]types.Storage, error) {
     storagelist, err := methods.GetStorageList(ctx, sts.RestAPITripper)
     return storagelist.Items, err
 }
 
 //FIXME TODO.WANGYONGCHAO
+func (sts *StorageService)GetDatastoreByName(ctx context.Context, name string) (*types.Storage, error) {
+    var storage *types.Storage
+    var err error
+
+    return storage, err
+}
+
 func (sts *StorageService)GetStorageInfoById(ctx context.Context, id string) (*types.Storage, error) {
     storage, err := methods.GetStorageInfo(ctx, sts.RestAPITripper, id)
     return storage, err
