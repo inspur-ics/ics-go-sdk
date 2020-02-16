95d0857d55e99203241a1c2d9080d2ec932dc87a
zhanghj <zhanghj.lc@inspur.com>
2020-02-15 22:21:10 -0500

    update code to get task state

--- common/client.go
+++ common/client.go
@@ -1,46 +1,45 @@
 package common
 
 import (
-    "context"
-    "github.com/inspur-ics/ics-go-sdk/client/methods"
-    "github.com/inspur-ics/ics-go-sdk/client/restful"
-    "github.com/inspur-ics/ics-go-sdk/client/types"
-    "sync"
-    "time"
+	"context"
+	"github.com/inspur-ics/ics-go-sdk/client/methods"
+	"github.com/inspur-ics/ics-go-sdk/client/restful"
+	"github.com/inspur-ics/ics-go-sdk/client/types"
+	"sync"
+	"time"
 )
 
-
-
 type RestAPI struct {
-    RestAPITripper restful.RestAPITripper
+	RestAPITripper restful.RestAPITripper
 }
 
 func (r *RestAPI) TraceTaskProcess(task *types.Task) (*types.TaskInfo, error) {
-    var wg sync.WaitGroup
-    wg.Add(1)
-    go r.tracing(task, wg)
-    wg.Wait()
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go r.tracing(task, &wg)
+	wg.Wait()
 
-    ctx := context.Background()
-    taskInfo, err := methods.GetTaskInfo(ctx, r.RestAPITripper, task)
-    return taskInfo, err
+	ctx := context.Background()
+	taskInfo, err := methods.GetTaskInfo(ctx, r.RestAPITripper, task)
+	return taskInfo, err
 }
 
-func (r *RestAPI) tracing(task *types.Task, wg sync.WaitGroup) {
-    defer wg.Done()
-    ctx := context.Background()
-    count := 1
-    for {
-        taskInfo, err := methods.GetTaskInfo(ctx, r.RestAPITripper, task)
-        if err != nil {
-            break
-        }
-        if taskInfo == nil || taskInfo.State == "FINISHED" || taskInfo.State == "ERROR" {
-            break
-        }
-        if count > 72000 {
-            break
-        }
-        time.Sleep(100 * time.Millisecond)
-    }
+func (r *RestAPI) tracing(task *types.Task, wg *sync.WaitGroup) {
+	defer wg.Done()
+	ctx := context.Background()
+	count := 1
+	for {
+		taskInfo, err := methods.GetTaskInfo(ctx, r.RestAPITripper, task)
+		if err != nil {
+			break
+		}
+		if taskInfo == nil || taskInfo.State == "FINISHED" || taskInfo.State == "ERROR" {
+			break
+		}
+		if count > 72000 {
+			break
+		}
+		time.Sleep(100 * time.Millisecond)
+		count++
+	}
 }
