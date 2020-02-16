0364b2eeae841b1622994fd41d2158380d809f40
zhanghj <zhanghj.lc@inspur.com>
2020-02-16 06:13:22 -0500

    add functions to set VM info

--- client/methods/vm.go
+++ client/methods/vm.go
@@ -1,148 +1,167 @@
 package methods
 
 import (
-    "context"
-    "encoding/json"
-    "fmt"
-    "github.com/inspur-ics/ics-go-sdk/client/restful"
-    "github.com/inspur-ics/ics-go-sdk/client/types"
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/inspur-ics/ics-go-sdk/client/restful"
+	"github.com/inspur-ics/ics-go-sdk/client/types"
 )
 
 func GetVMById(ctx context.Context, r restful.RestAPITripper, vmId string) (*types.VirtualMachine, error) {
-    var reqBody      *types.Common
-    var api          types.ICSApi
-    var response = types.VirtualMachine{}
+	var reqBody *types.Common
+	var api types.ICSApi
+	var response = types.VirtualMachine{}
 
-    if len(vmId) <= 0 {
-        vmId = "anonymous"
-    }
-    api.Api = fmt.Sprintf("/vms/%s",vmId)
-    api.Token = true
+	if len(vmId) <= 0 {
+		vmId = "anonymous"
+	}
+	api.Api = fmt.Sprintf("/vms/%s", vmId)
+	api.Token = true
 
-    resp, err := r.GetTrip(ctx, api, reqBody)
-    respBody, err1 := HandleResponse(resp, err)
-    if err1 != nil {
-        err = err1
-    } else if respBody != nil {
-        jsonErr := json.Unmarshal([]byte(respBody), &response)
-        err = JsonError(jsonErr)
-    }
+	resp, err := r.GetTrip(ctx, api, reqBody)
+	respBody, err1 := HandleResponse(resp, err)
+	if err1 != nil {
+		err = err1
+	} else if respBody != nil {
+		jsonErr := json.Unmarshal([]byte(respBody), &response)
+		err = JsonError(jsonErr)
+	}
 
-    return &response, err
+	return &response, err
 }
 
 func GetVMByIP(ctx context.Context, r restful.RestAPITripper, ipAddy string) (*types.VirtualMachine, error) {
-    var reqBody      *types.Common
-    var api          types.ICSApi
-    var vmPages = types.VMPageResponse{}
-    var response = types.VirtualMachine{}
+	var reqBody *types.Common
+	var api types.ICSApi
+	var vmPages = types.VMPageResponse{}
+	var response = types.VirtualMachine{}
 
-    if len(ipAddy) <= 6 {
-        ipAddy = "255.255.255.255"
-    }
-    api.Api = fmt.Sprintf("/vms?pageSize=1&currentPage=1&sortField=&sort=desc&vnicIp=%s",ipAddy)
-    api.Token = true
+	if len(ipAddy) <= 6 {
+		ipAddy = "255.255.255.255"
+	}
+	api.Api = fmt.Sprintf("/vms?pageSize=1&currentPage=1&sortField=&sort=desc&vnicIp=%s", ipAddy)
+	api.Token = true
 
-    resp, err := r.GetTrip(ctx, api, reqBody)
-    respBody, err1 := HandleResponse(resp, err)
-    if err1 != nil {
-        err = err1
-    } else if respBody != nil {
-        jsonErr := json.Unmarshal([]byte(respBody), &vmPages)
-        err = JsonError(jsonErr)
-    }
-    if vmPages.TotalSize == 1 {
-        response = vmPages.Items[0]
-    }
+	resp, err := r.GetTrip(ctx, api, reqBody)
+	respBody, err1 := HandleResponse(resp, err)
+	if err1 != nil {
+		err = err1
+	} else if respBody != nil {
+		jsonErr := json.Unmarshal([]byte(respBody), &vmPages)
+		err = JsonError(jsonErr)
+	}
+	if vmPages.TotalSize == 1 {
+		response = vmPages.Items[0]
+	}
 
-    return &response, err
+	return &response, err
 }
 
 func GetVMByName(ctx context.Context, r restful.RestAPITripper, name string) (*types.VirtualMachine, error) {
-    var reqBody      *types.Common
-    var api          types.ICSApi
-    var vmPages = types.VMPageResponse{}
-    var response = types.VirtualMachine{}
+	var reqBody *types.Common
+	var api types.ICSApi
+	var vmPages = types.VMPageResponse{}
+	var response = types.VirtualMachine{}
 
-    if len(name) <= 0 {
-        name = "vm@anonymous"
-    }
-    api.Api = fmt.Sprintf("/vms?pageSize=1&currentPage=1&sortField=&sort=desc&name=%s",name)
-    api.Token = true
+	if len(name) <= 0 {
+		name = "vm@anonymous"
+	}
+	api.Api = fmt.Sprintf("/vms?pageSize=1&currentPage=1&sortField=&sort=desc&name=%s", name)
+	api.Token = true
 
-    resp, err := r.GetTrip(ctx, api, reqBody)
-    respBody, err1 := HandleResponse(resp, err)
-    if err1 != nil {
-        err = err1
-    } else if respBody != nil {
-        jsonErr := json.Unmarshal([]byte(respBody), &vmPages)
-        err = JsonError(jsonErr)
-    }
-    if vmPages.TotalSize == 1 {
-        response = vmPages.Items[0]
-    }
+	resp, err := r.GetTrip(ctx, api, reqBody)
+	respBody, err1 := HandleResponse(resp, err)
+	if err1 != nil {
+		err = err1
+	} else if respBody != nil {
+		jsonErr := json.Unmarshal([]byte(respBody), &vmPages)
+		err = JsonError(jsonErr)
+	}
+	if vmPages.TotalSize == 1 {
+		response = vmPages.Items[0]
+	}
 
-    return &response, err
+	return &response, err
 }
 
 func GetVMPageList(ctx context.Context, r restful.RestAPITripper, req *types.VMPageReq) (*types.VMPageResponse, error) {
-    var api          types.ICSApi
-    var response = types.VMPageResponse{}
+	var api types.ICSApi
+	var response = types.VMPageResponse{}
 
-    api.Api = "/vms"
-    api.Token = true
+	api.Api = "/vms"
+	api.Token = true
 
-    resp, err := r.GetTrip(ctx, api, req)
-    respBody, err1 := HandleResponse(resp, err)
-    if err1 != nil {
-        err = err1
-    } else if respBody != nil {
-        jsonErr := json.Unmarshal([]byte(respBody), &response)
-        err = JsonError(jsonErr)
-    }
+	resp, err := r.GetTrip(ctx, api, req)
+	respBody, err1 := HandleResponse(resp, err)
+	if err1 != nil {
+		err = err1
+	} else if respBody != nil {
+		jsonErr := json.Unmarshal([]byte(respBody), &response)
+		err = JsonError(jsonErr)
+	}
 
-    return &response, err
+	return &response, err
 }
 
 func PowerOnVMById(ctx context.Context, r restful.RestAPITripper, vmId string) (*types.Task, error) {
-    var reqBody      *types.Common
-    var api          types.ICSApi
-    var response = types.Task{}
+	var reqBody *types.Common
+	var api types.ICSApi
+	var response = types.Task{}
 
-    if len(vmId) <= 0 {
-        vmId = "anonymous"
-    }
-    api.Api = fmt.Sprintf("/vms/%s?action=poweron",vmId)
-    api.Token = true
+	if len(vmId) <= 0 {
+		vmId = "anonymous"
+	}
+	api.Api = fmt.Sprintf("/vms/%s?action=poweron", vmId)
+	api.Token = true
 
-    resp, err := r.PutTrip(ctx, api, reqBody)
-    respBody, err1 := HandleResponse(resp, err)
-    if err1 != nil {
-        err = err1
-    } else if respBody != nil {
-        jsonErr := json.Unmarshal([]byte(respBody), &response)
-        err = JsonError(jsonErr)
-    }
+	resp, err := r.PutTrip(ctx, api, reqBody)
+	respBody, err1 := HandleResponse(resp, err)
+	if err1 != nil {
+		err = err1
+	} else if respBody != nil {
+		jsonErr := json.Unmarshal([]byte(respBody), &response)
+		err = JsonError(jsonErr)
+	}
 
-    return &response, err
+	return &response, err
 }
 
 func GetVMList(ctx context.Context, r restful.RestAPITripper) (*types.VMPageResponse, error) {
-    var reqBody      *types.Common
-    var api          types.ICSApi
-    var response = types.VMPageResponse{}
+	var reqBody *types.Common
+	var api types.ICSApi
+	var response = types.VMPageResponse{}
 
-    api.Api = "/vms/"
-    api.Token = true
+	api.Api = "/vms/"
+	api.Token = true
 
-    resp, err := r.GetTrip(ctx, api, reqBody)
-    respBody, err1 := HandleResponse(resp, err)
-    if err1 != nil {
-        err = err1
-    } else if respBody != nil {
-        jsonErr := json.Unmarshal([]byte(respBody), &response)
-        err = JsonError(jsonErr)
-    }
+	resp, err := r.GetTrip(ctx, api, reqBody)
+	respBody, err1 := HandleResponse(resp, err)
+	if err1 != nil {
+		err = err1
+	} else if respBody != nil {
+		jsonErr := json.Unmarshal([]byte(respBody), &response)
+		err = JsonError(jsonErr)
+	}
 
-    return &response, err
+	return &response, err
+}
+
+func SetVM(ctx context.Context, r restful.RestAPITripper, vmInfo types.VirtualMachine) (*types.Task, error) {
+	var api types.ICSApi
+	var response = types.Task{}
+
+	api.Api = fmt.Sprintf("/vms/%s", vmInfo.ID)
+	api.Token = true
+
+	resp, err := r.PutTrip(ctx, api, vmInfo)
+	respBody, err1 := HandleResponse(resp, err)
+	if err1 != nil {
+		err = err1
+	} else if respBody != nil {
+		jsonErr := json.Unmarshal([]byte(respBody), &response)
+		err = JsonError(jsonErr)
+	}
+
+	return &response, err
 }
--- vm/virtual_machine.go
+++ vm/virtual_machine.go
@@ -1,54 +1,59 @@
 package vm
 
 import (
-    "context"
-    "github.com/inspur-ics/ics-go-sdk/client/methods"
-    "github.com/inspur-ics/ics-go-sdk/client/types"
+	"context"
+	"github.com/inspur-ics/ics-go-sdk/client/methods"
+	"github.com/inspur-ics/ics-go-sdk/client/types"
 )
 
 func (v *VirtualMachineService) GetVM(ctx context.Context, id string) (*types.VirtualMachine, error) {
-   vm, err := methods.GetVMById(ctx, v.RestAPITripper, id)
-   return vm, err
+	vm, err := methods.GetVMById(ctx, v.RestAPITripper, id)
+	return vm, err
+}
+
+func (v *VirtualMachineService) SetVM(ctx context.Context, vmInfo types.VirtualMachine) (*types.Task, error) {
+	task, err := methods.SetVM(ctx, v.RestAPITripper, vmInfo)
+	return task, err
 }
 
 func (v *VirtualMachineService) GetVMList(ctx context.Context) ([]types.VirtualMachine, error) {
-    vmlist, err := methods.GetVMList(ctx, v.RestAPITripper)
-    return vmlist.Items, err
+	vmlist, err := methods.GetVMList(ctx, v.RestAPITripper)
+	return vmlist.Items, err
 }
 
 func (v *VirtualMachineService) VMPageList(req *types.VMPageReq) (*types.VMPageResponse, error) {
-    ctx := context.Background()
-    vmPages, err := methods.GetVMPageList(ctx, v.RestAPITripper, req)
-    return vmPages, err
+	ctx := context.Background()
+	vmPages, err := methods.GetVMPageList(ctx, v.RestAPITripper, req)
+	return vmPages, err
 }
 
 func (v *VirtualMachineService) GetVMByUUID(ctx context.Context, vmUUID string) (*types.VirtualMachine, error) {
-    vms, err := v.GetVMList(ctx)
-    for _, vm := range vms {
-        if vmUUID == vm.UUID{
-            return &vm, err
-        }
-    }
-    return nil, err
+	vms, err := v.GetVMList(ctx)
+	for _, vm := range vms {
+		if vmUUID == vm.UUID {
+			return &vm, err
+		}
+	}
+	return nil, err
 }
 
 func (v *VirtualMachineService) GetVMByIP(ctx context.Context, ipAddy string) (*types.VirtualMachine, error) {
-    vm, err := methods.GetVMByIP(ctx, v.RestAPITripper, ipAddy)
-    return vm, err
+	vm, err := methods.GetVMByIP(ctx, v.RestAPITripper, ipAddy)
+	return vm, err
 }
 
 func (v *VirtualMachineService) GetVMByName(ctx context.Context, name string) (*types.VirtualMachine, error) {
-    vm, err := methods.GetVMByName(ctx, v.RestAPITripper, name)
-    return vm, err
+	vm, err := methods.GetVMByName(ctx, v.RestAPITripper, name)
+	return vm, err
 }
 
 func (v *VirtualMachineService) GetVMByPath(ctx context.Context, path string) (*types.VirtualMachine, error) {
-    vm, err := methods.GetVMById(ctx, v.RestAPITripper, path)
-    return vm, err
+	vm, err := methods.GetVMById(ctx, v.RestAPITripper, path)
+	return vm, err
 }
 
 func (v *VirtualMachineService) PowerOnVM(id string) (*types.Task, error) {
-   ctx := context.Background()
-   task, err := methods.PowerOnVMById(ctx, v.RestAPITripper, id)
-   return task, err
+	ctx := context.Background()
+	task, err := methods.PowerOnVMById(ctx, v.RestAPITripper, id)
+	return task, err
 }
