88dfd5ad56107ad224f3549396cef2138ee4e163
zhanghuijian <zhanghj.lc@inspur.com>
2021-03-25 17:55:41 +0800

    add ScsiID in types.Volume

--- volume/volume_test.go
+++ volume/volume_test.go
@@ -1,12 +1,12 @@
 package volume
 
 import (
-    "context"
-    "fmt"
-    icsgo "github.com/inspur-ics/ics-go-sdk"
-    "github.com/inspur-ics/ics-go-sdk/client/types"
-    "testing"
-    "time"
+	"context"
+	"fmt"
+	icsgo "github.com/inspur-ics/ics-go-sdk"
+	"github.com/inspur-ics/ics-go-sdk/client/types"
+	"testing"
+	"time"
 )
 
 var volumeClient *VolumeService
@@ -41,18 +41,18 @@ func TestCreateVolume(t *testing.T) {
 	volumeClient = NewVolumeService(icsConnection.Client)
 	task, err := volumeClient.CreateVolume(ctx, createVolumeReq)
 	fmt.Println(task.TaskId)
-    time.Sleep(5*time.Second)
+	time.Sleep(5 * time.Second)
 }
 
 func TestGetVolumesInDatastore(t *testing.T) {
 	fmt.Println("********************GetVolumesInDatastore**************")
 	volumes, err := volumeClient.GetVolumesInDatastore(ctx, "8a878bda6f6f3ca4016f6f458b50003c")
 	if volumes != nil {
-		for i := 0; i <len(volumes); {
+		for i := 0; i < len(volumes); {
 			volumeinfo := volumes[i]
 			if volumeinfo.Name == "test" {
 				volumeid = volumeinfo.ID
-				fmt.Println("test volume id :"+volumeid)
+				fmt.Println("test volume id :" + volumeid)
 			}
 			i++
 		}
@@ -61,10 +61,19 @@ func TestGetVolumesInDatastore(t *testing.T) {
 	}
 }
 func TestGetVolumeInfoById(t *testing.T) {
+	icsConnection = icsgo.ICSConnection{
+		Username: "admin",
+		Password: "Cloud@s1",
+		Hostname: "10.48.50.13",
+		Port:     "443",
+		Insecure: true,
+	}
+	ctx = context.Background()
+	icsConnection.Connect(ctx)
+	volumeClient = NewVolumeService(icsConnection.Client)
 	fmt.Println("********************TestGetVolumeInfoById**************")
-	volumeinfo, err := volumeClient.GetVolumeInfoById(ctx, volumeid)
+	volumeinfo, err := volumeClient.GetVolumeInfoById(ctx, "8ab0b28d7867fb1d0178687e6c13006a")
 	if err == nil {
-
 		fmt.Println(volumeinfo.Name)
 	} else {
 		fmt.Println(err.Error())
@@ -72,12 +81,12 @@ func TestGetVolumeInfoById(t *testing.T) {
 }
 func TestDeleteVolume(t *testing.T) {
 	fmt.Println("********************TestDeleteVolume**************")
-    if volumeid!= "" {
-        task, err := volumeClient.DeleteVolume(ctx, volumeid,true)
-        if err == nil {
-            fmt.Println(task.TaskId)
-        } else {
-            fmt.Println(err.Error())
-        }
-    }
+	if volumeid != "" {
+		task, err := volumeClient.DeleteVolume(ctx, volumeid, true)
+		if err == nil {
+			fmt.Println(task.TaskId)
+		} else {
+			fmt.Println(err.Error())
+		}
+	}
 }
